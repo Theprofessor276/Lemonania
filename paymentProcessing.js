bde17d71c0a186b340bf7716ca514e2d27810971
Theprofessor276 <122837364+Theprofessor276@users.noreply.github.com>
2025-11-16 00:10:26 -0600

    Uh oh i didn't publish that change I made 2 months ago

--- paymentProcessing.js
+++ paymentProcessing.js
@@ -3,3 +3,4 @@ import { getBankBalance } from './debug.js';
 const user = getCurrentUser();
 const balance = getBankBalance(user);
 document.getElementById('balance').innerHTML = `$${balance.toFixed(2)}`;
+// boo
